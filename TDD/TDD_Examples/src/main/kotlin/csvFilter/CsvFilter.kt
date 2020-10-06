eb3e78e7ce2042295ec3cc664998487a481dc7ca
Abian Sanchez <AbianSanchezM@gmail.com>
2020-10-06 22:45:47 +0100

    Fourth test failed, not contemplated when a tax is not a number

--- TDD/TDD_Examples/src/main/kotlin/csvFilter/CsvFilter.kt
+++ TDD/TDD_Examples/src/main/kotlin/csvFilter/CsvFilter.kt
@@ -6,9 +6,12 @@ class CsvFilter {
         result.add(lines[0])
         val invoice = lines[1]
         val fields = invoice.split(',')
-        if (fields[4].isNullOrEmpty() || fields[5].isNullOrEmpty()){
-            if (!(fields[4].isNullOrEmpty() && fields[5].isNullOrEmpty()))
-                result.add(lines[1])
+        val ivaFieldIndex = 4
+        val igicFieldIndex = 5
+        val bothTaxFieldsAreAdded = (fields[ivaFieldIndex].isNullOrEmpty() || fields[igicFieldIndex].isNullOrEmpty())
+        val bothTaxFieldsAreEmpty = !(fields[ivaFieldIndex].isNullOrEmpty() && fields[igicFieldIndex].isNullOrEmpty())
+        if (bothTaxFieldsAreAdded && bothTaxFieldsAreEmpty){
+            result.add(lines[1])
         }
         return result.toList()
     }
